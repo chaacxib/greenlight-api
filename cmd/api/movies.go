1999305d9a910202acbde2ce77540ea25f063e44
ChaacXib <chaacxib@skiff.com>
2023-01-24 16:45:54 -0600

    Add requests helpers

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // "POST /v1/movies" endpoint.
@@ -15,15 +12,9 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 // "GET /v1/movies/:id" endpoint.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// When httprouter is parsing a request, any interpolated URL parameters will be
-	// stored in the request context. We can use the ParamsFromContext() function to
-	// retrieve a slice containing these parameter names and values.
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// We can then use the ByName() method to get the value of the "id" parameter from
-	// the slice.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
+		app.logger.Printf(err.Error())
 		http.NotFound(w, r)
 		return
 	}
