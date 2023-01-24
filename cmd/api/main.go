a1eb82b79aa94a19106f45dc9898349e9e30af4a
ChaacXib <chaacxib@skiff.com>
2023-01-24 16:22:21 -0600

    Add logs for future reference

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,23 +11,29 @@ import (
 
 const version = "1.0.0"
 
+// config struct to hold all the configuration settings for our application.
 type config struct {
 	port int
 	env  string
 }
 
+// application struct to hold the dependencies for our HTTP handlers, helpers and middleware.
 type application struct {
 	config config
 	logger *log.Logger
 }
 
 func main() {
+	// Declare an instance of the config struct.
 	var cfg config
 
+	// Read the value of the port and env command-line flags into the config struct.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
+	// Declare an instance of the application struct, containing the config struct and
+	// the logger.
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	app := &application{
 		config: cfg,
@@ -37,6 +43,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 
+	// Declare a HTTP server with some sensible timeout settings
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      mux,
@@ -45,6 +52,7 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 
+	// Start the HTTP server.
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
