a49df54b5bae9e0af9141706ef236b2c8fbe1bb7
ChaacXib <chaacxib@skiff.com>
2023-02-02 16:31:29 -0600

    Add custom error handlers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,11 @@ func (app *application) routes() *httprouter.Router {
 	// Initialize a new httprouter router instance.
 	router := httprouter.New()
 
+	// Set custom error handlers for 404 (Not Found)
+	// and 405 (Method Not Allowed) responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register the relevant methods, URL patterns and handler functions for our
 	// endpoints using the HandlerFunc() method.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
