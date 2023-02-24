ddfcc8743cdbcc7f9d20ae34353287081cf50138
ChaacXib <chaacxib@skiff.com>
2023-02-23 20:13:25 -0600

    Add movies crud db connection skeleton

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,8 @@ import (
 	"os"
 	"time"
 
+	"greenlight.chaacxib.hi/internal/data"
+
 	_ "github.com/lib/pq"
 )
 
@@ -31,6 +33,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -66,6 +69,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	// Declare a HTTP server with some sensible timeout settings
